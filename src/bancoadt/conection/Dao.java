a144e1962c7eaf69dcf0dc5996ce685178c6fd13
Bot-Steven <83646314+Bot-Steven@users.noreply.github.com>
2021-09-20 12:31:39 +0200

    Clases de las entidades creadas y la interfaz del DAO en proceso

---
+++ src/bancoadt/conection/Dao.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bancoadt.conection;
+
+import bancoadt.clases.Customer;
+
+/**
+ *
+ * @author 2dam
+ */
+public interface Dao {
+    
+    public void crearCliente(Customer cust);
+    
+}
