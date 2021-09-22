b8cd0d80b2eb798f83e31603eae879f8152e32b9
Bot-Steven <83646314+Bot-Steven@users.noreply.github.com>
2021-09-22 09:03:18 +0200

    Segundo commit con los atributos de todas las entidades de la BD y la interfaz DAO con su implementacion

--- src/bancoadt/clases/Movement.java
+++ src/bancoadt/clases/Movement.java
@@ -5,10 +5,57 @@
  */
 package bancoadt.clases;
 
+import java.time.LocalDate;
+
 /**
  *
  * @author Steven,Irkus,Unai y Adrian
  */
 public class Movement {
+    
+    private int id;
+    private LocalDate timeStamp;
+    private float amount,balance;
+    private String description;
+    
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTimeStamp(LocalDate timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+
+    public void setAmount(float amount) {
+        this.amount = amount;
+    }
+
+    public void setBalance(float balance) {
+        this.balance = balance;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public LocalDate getTimeStamp() {
+        return timeStamp;
+    }
+
+    public float getAmount() {
+        return amount;
+    }
+
+    public float getBalance() {
+        return balance;
+    }
+
+    public String getDescription() {
+        return description;
+    }
     
 }
